d782b2c32755d0bdecf41f87fadacbf06920dd4c
Muhammad Haris Javed <71623701+harisjaved1996@users.noreply.github.com>
2026-05-19 11:54:44 +0500

    Remove LWC Components

--- dpeg/force-app/main/default/lwc/siteFooter/siteFooter.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class SiteFooter extends LightningElement {}
--- dpeg/force-app/main/default/lwc/siteHero/siteHero.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class SiteHero extends LightningElement {}
